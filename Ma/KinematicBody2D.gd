0edeecf69c10deea6f6363a744fffca678ec8a0e
Yan-kuudere <92384442+Yan-kuudere@users.noreply.github.com>
2022-01-29 13:51:05 +0700

    Add files via upload
    Vẫn là ma nhưng được nâng cấp :)))))))))

--- Ma/KinematicBody2D.gd
+++ Ma/KinematicBody2D.gd
@@ -2,21 +2,24 @@ extends KinematicBody2D
 
 var motion = Vector2()
 
-var state = 0
+var state = 1 
 
 func _physics_process(_delta):
-	if state == 0:
-		motion.x = 0
-	elif state == 1:
+	if state == 1:
 		motion.x = 100
+		if $Right.is_colliding():
+			 state = 2
 	elif state == 2:
 		motion.x = -100
+		if $Left.is_colliding():
+			state = 3
 	elif state == 3:
 		motion.y = 100
+		if $Down.is_colliding():
+			state = 4
 	elif state == 4:
 		motion.y = -100
+		if $Up.is_colliding():
+			state = 1
 	print(state)
 	motion = move_and_slide(motion, Vector2(0,-1))
-
-func _on_Timer_timeout():
-	state = floor(rand_range(0,5))
